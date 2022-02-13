de80f6f18102a6e97dd9978b85f7fd07f71972a1
Julian Nonino <noninojulian@gmail.com>
2022-02-13 11:53:28 +0000

    Update AWS privider

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,12 +1,10 @@
 module "cluster" {
-  source  = "cn-terraform/ecs-cluster/aws"
-  version = "1.0.7"
-  name    = "test-cluster"
+  source = "cn-terraform/ecs-cluster/aws"
+  name   = "test-cluster"
 }
 
 module "base-network" {
   source                                      = "cn-terraform/networking/aws"
-  version                                     = "2.0.13"
   name_prefix                                 = "test-networking"
   vpc_cidr_block                              = "192.168.0.0/16"
   availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
@@ -16,16 +14,13 @@ module "base-network" {
 
 module "td" {
   source          = "cn-terraform/ecs-fargate-task-definition/aws"
-  version         = "1.0.24"
   name_prefix     = "test-td"
   container_image = "ubuntu"
   container_name  = "test"
 }
 
 module "service" {
-  source  = "cn-terraform/ecs-fargate-service/aws"
-  version = "2.0.18"
-
+  source              = "cn-terraform/ecs-fargate-service/aws"
   name_prefix         = "test-service"
   vpc_id              = module.base-network.vpc_id
   ecs_cluster_arn     = module.cluster.aws_ecs_cluster_cluster_arn
@@ -47,4 +42,3 @@ module "ecs-service-autoscaling" {
   # public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
   # private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
 }
-
