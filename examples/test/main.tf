d1db0dc4b9db9a5fb73f37ad05c0948d2a2d07fb
Julian Nonino <noninojulian@gmail.com>
2024-04-09 13:31:24 +0100

    Fix examples

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -4,12 +4,45 @@ module "cluster" {
 }
 
 module "base-network" {
-  source                                      = "cn-terraform/networking/aws"
-  name_prefix                                 = "test-networking"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  source     = "cn-terraform/networking/aws"
+  cidr_block = "192.168.0.0/16"
+
+  vpc_additional_tags = {
+    vpc_tag1 = "tag1",
+    vpc_tag2 = "tag2",
+  }
+
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+  }
+
+  public_subnets_additional_tags = {
+    public_subnet_tag1 = "tag1",
+    public_subnet_tag2 = "tag2",
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+  }
+
+  private_subnets_additional_tags = {
+    private_subnet_tag1 = "tag1",
+    private_subnet_tag2 = "tag2",
+  }
 }
 
 module "td" {
@@ -25,8 +58,8 @@ module "service" {
   vpc_id              = module.base-network.vpc_id
   ecs_cluster_arn     = module.cluster.aws_ecs_cluster_cluster_arn
   task_definition_arn = module.td.aws_ecs_task_definition_td_arn
-  public_subnets      = module.base-network.public_subnets_ids
-  private_subnets     = module.base-network.private_subnets_ids
+  public_subnets      = module.base-network.public_subnets
+  private_subnets     = module.base-network.private_subnets
   container_name      = "test"
   enable_autoscaling  = false
 }
